4ef0b892522e5f0b1bc331f266604aca67532a9b
Mario Burdallo Camargo <147068334+Burdalloo@users.noreply.github.com>
2025-11-18 16:42:26 +0100

    Implementacion de las clases del Componente 2 (Catalogacion y registros bibliograficos)

---
+++ src/Servidor/Componente02_CDU19/CatalogoService.java
@@ -0,0 +1,32 @@
+package Servidor;
+
+public class CatalogoService {
+
+	/**
+	 * 
+	 * @param query
+	 */
+	public List<RecursoDTO> buscar(int query) {
+		// TODO - implement CatalogoService.buscar
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idAsig
+	 */
+	public List<RecursoDTO> buscarPorAsignatura(int idAsig) {
+		// TODO - implement CatalogoService.buscarPorAsignatura
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param solicitud
+	 */
+	public void solicitarAdquisicion(int solicitud) {
+		// TODO - implement CatalogoService.solicitarAdquisicion
+		throw new UnsupportedOperationException();
+	}
+
+}
