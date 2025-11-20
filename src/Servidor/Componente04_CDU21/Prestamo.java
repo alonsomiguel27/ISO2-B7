845caa416316c4d2634a93d326b1343f63773bb5
marcosshc <marcos.heredia@alu.uclm.es>
2025-11-20 11:13:38 +0100

    Add files via upload
    Add ReservasView to Componente04_CDU21

---
+++ src/Servidor/Componente04_CDU21/Prestamo.java
@@ -0,0 +1,16 @@
+package Servidor;
+
+public class Prestamo {
+	private String _idPrestamo;
+	private String _fechaInicio;
+	private String _fechaVencimiento;
+	public Usuario _.N;
+
+	public double calcularMulta() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean renovar() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/Servidor/Componente04_CDU21/PrestamoService.java
@@ -0,0 +1,20 @@
+package Servidor;
+
+public class PrestamoService {
+
+	public PrestamoDTO realizarPrestamo(Object aIdUsuario, Object aIdItem) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void registrarDevolucion(Object aIdItem) {
+		throw new UnsupportedOperationException();
+	}
+
+	public PrestamoDTO renovarPrestamo(Object aIdPrestamo) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<PrestamoDTO> consultarHistorial(Object aIdUsuario) {
+		throw new UnsupportedOperationException();
+	}
+}
