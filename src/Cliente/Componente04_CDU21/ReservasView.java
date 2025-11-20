2afef2202e84fac9ec199e692405bf8de0fd1624
marcosshc <marcos.heredia@alu.uclm.es>
2025-11-20 11:12:55 +0100

    Add files via upload
    Add ReservasView to Componente04_CDU21

---
+++ src/Cliente/Componente04_CDU21/ReservasView.java
@@ -0,0 +1,9 @@
+package Cliente;
+
+public class ReservasView {
+	private List<ReservaDTO> _reservasActivas;
+
+	public void onCancelarReserva(Object aIdReserva) {
+		throw new UnsupportedOperationException();
+	}
+}
