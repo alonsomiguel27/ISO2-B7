4f06382939dfd5610b4ea12437e96e989fb1afd3
Hugo <hugo.arevalo@alu.uclm.es>
2025-11-28 23:12:44 +0100

    Correción de iteración 3 en servidor. Closes #24

---
+++ Servidor/componente03_cdu20/src/main/Recurso.java
@@ -0,0 +1,11 @@
+package Servidor;
+
+public class Recurso {
+
+    private String idRecurso;
+    private String titulo;
+
+    public boolean comprobarDisponibilidad() {
+        return false;
+    }
+}
