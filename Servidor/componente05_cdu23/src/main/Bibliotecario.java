8ed83161f562824a51715fb31a6f820acab332c3
alonso <alonso.miguel@alu.uclm.es>
2025-11-30 20:23:51 +0100

    corrección implementación componente 5 servidor

---
+++ Servidor/componente05_cdu23/src/main/Bibliotecario.java
@@ -0,0 +1,10 @@
+package Servidor;
+
+public class Bibliotecario extends Usuario {
+
+	public void generarInformes() {
+		// TODO - implement Bibliotecario.generarInformes
+		throw new UnsupportedOperationException();
+	}
+
+}
