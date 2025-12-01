8c0395a8d54db0e1851fbd83957f97b6fb4d3513
arturodomenechh <arturo.domenech@alu.uclm.es>
2025-12-01 19:25:18 +0100

    Implementación Componente07_CDU25 Cliente. Closes #30

---
+++ Cliente/componente07_cdu25/src/main/java/es/UCLM/esi/ISO2/B7/GestionProyectosView.java
@@ -0,0 +1,16 @@
+package es.UCLM.esi.ISO2.B7;
+
+import java.util.List;
+
+public class GestionProyectosView {
+    private List<ProyectoDTO> proyectos;
+    private List<RecursoDTO> suscripciones;
+
+    public void onAddSuscripcion(String idProyecto, String idRecurso) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void onAccederRestriccion(String idProyecto) {
+        throw new UnsupportedOperationException();
+    }
+}
