76a3aa7329e044c0085d340dfbabc636fc2f8528
Burdallo <Mario.Burdallo@alu.uclm.es>
2025-12-01 20:00:01 +0100

    Implementación del componente 8. Closes #33

---
+++ Servidor/componente08_cdu26/src/main/Rol.java
@@ -0,0 +1,17 @@
+package Servidor;
+
+public class Rol {
+
+	private String idRol;
+	private String nombre;
+
+	/**
+	 * 
+	 * @param perm
+	 */
+	public void asignarPermiso(int perm) {
+		// TODO - implement Rol.asignarPermiso
+		throw new UnsupportedOperationException();
+	}
+
+}
