9fa9d01136d82c7d7207a4f61310f3a5a83e04d0
marcosshc <marcos.heredia@alu.uclm.es>
2025-12-02 21:18:14 +0100

    Añadido componente10_cdu28 (CDU 28) - IntegracionService

---
+++ Servidor/componente10_cdu28/src/main/java/es/UCLM/esi/ISO2/B7/IntegracionService.java
@@ -0,0 +1,36 @@
+package es.UCLM.esi.ISO2.B7;
+
+/**
+ * IntegracionService
+ * Componente 10 - Servidor (CDU 28)
+ * Motor de procesamiento y agregación de datos.
+ */
+public class IntegracionService {
+
+    /**
+     * Default constructor
+     */
+    public IntegracionService() {
+    }
+
+    /**
+     * Sincroniza alumnos con sistemas externos.
+     */
+    public void sincronizarAlumnos() {
+    }
+
+    /**
+     * Genera estadísticas globales.
+     * @return ReporteDTO
+     */
+    public ReporteDTO generarEstadisticasGlobales() {
+        return null;
+    }
+
+    /**
+     * Busca actualizaciones en sistemas externos.
+     */
+    public void buscarActualizaciones() {
+    }
+
+}
