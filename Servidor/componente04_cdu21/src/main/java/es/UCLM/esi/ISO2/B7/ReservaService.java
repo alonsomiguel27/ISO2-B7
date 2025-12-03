caa9e8813e1d5081773f121abe1520e03b686609
marcosshc <marcos.heredia@alu.uclm.es>
2025-12-03 01:56:22 +0100

    Corregido componente 04 (issue #25)

---
+++ Servidor/componente04_cdu21/src/main/java/es/UCLM/esi/ISO2/B7/ReservaService.java
@@ -0,0 +1,14 @@
+package es.UCLM.esi.ISO2.B7;
+
+public class ReservaService {
+
+    public ReservaService() { }
+
+    public ReservaDTO crearReserva(String idUsuario, String idRecurso) {
+        return null;
+    }
+
+    public void gestionarColaReservas(String idRecurso) { }
+
+    public void cancelarReserva(String idReserva) { }
+}
