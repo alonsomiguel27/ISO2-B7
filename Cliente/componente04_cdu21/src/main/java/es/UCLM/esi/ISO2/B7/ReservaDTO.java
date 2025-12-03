5cace10a34ca772b999c5fab66b69c33f5493032
marcosshc <marcos.heredia@alu.uclm.es>
2025-12-03 02:13:54 +0100

    Corregido componente 04 cliente (issue #25)

---
+++ Cliente/componente04_cdu21/src/main/java/es/UCLM/esi/ISO2/B7/ReservaDTO.java
@@ -0,0 +1,14 @@
+package es.UCLM.esi.ISO2.B7;
+
+public class ReservaDTO {
+
+    private String idReserva;
+
+    public ReservaDTO(String idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public String getIdReserva() {
+        return idReserva;
+    }
+}
