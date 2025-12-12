8d45f2d25b3808255986b212075c890ade3bbd25
Hugo <hugo.arevalo@alu.uclm.es>
2025-12-12 12:07:40 +0100

    Iteración 21 en cliente, retraso de un día. Closes #58

---
+++ Cliente/componente21_cdu6/target/test-classes/AppTest.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class AppTest {
+   public AppTest() {
+   }
+
+   @Test
+   public void shouldAnswerWithTrue() {
+      throw new Error("Unresolved compilation problems: \n\tTest cannot be resolved to a type\n\tThe method assertTrue(boolean) is undefined for the type AppTest\n");
+   }
+}
