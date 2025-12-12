ab6f658024e9be4a601cb290055853a718aeb40d
Hugo <hugo.arevalo@alu.uclm.es>
2025-12-12 11:59:03 +0100

    Iteración 21, se ha realizado con retraso de un día. Closes #59

---
+++ Servidor/componente21_cdu6/src/main/SolicitudAdquisicion.java
@@ -0,0 +1,19 @@
+package Servidor;
+
+public class SolicitudAdquisicion {
+
+	private String idSolicitud;
+	private String titulo;
+	private String justificacion;
+
+	public void aprobar() {
+		// TODO - implement SolicitudAdquisicion.aprobar
+		throw new UnsupportedOperationException();
+	}
+
+	public void rechazar() {
+		// TODO - implement SolicitudAdquisicion.rechazar
+		throw new UnsupportedOperationException();
+	}
+
+}
