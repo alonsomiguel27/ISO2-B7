5b44cdfacbe47a8704a109a33275758643780f0b
marcosshc <marcos.heredia@alu.uclm.es>
2025-12-12 18:17:34 +0100

    implementación componente 22 servidor. Closes #61

---
+++ Servidor/componente22_cdu10/src/main/java/es/UCLM/esi/ISO2/B7/GruposService.java
@@ -0,0 +1,16 @@
+package es.UCLM.esi.ISO2.B7;
+
+public class GruposService {
+
+    public void crearGrupo(String nombreGrupo, String idAsignaturaOTema) {
+    }
+
+    public void eliminarGrupo(String idGrupo) {
+    }
+
+    public void añadirUsuarioAGrupo(String idGrupo, String idUsuario) {
+    }
+
+    public void eliminarUsuarioDeGrupo(String idGrupo, String idUsuario) {
+    }
+}
