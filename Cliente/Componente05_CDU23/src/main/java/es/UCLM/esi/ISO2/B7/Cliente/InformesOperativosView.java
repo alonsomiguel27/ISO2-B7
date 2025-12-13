521bc2470ecaba1ca3d30b699be7ea43d573b97e
Maanu24sc <manuel.sanchez33@alu.uclm.es>
2025-12-13 20:55:41 +0100

    Implementación componente 23 CLiente. #62

---
+++ Cliente/Componente05_CDU23/src/main/java/es/UCLM/esi/ISO2/B7/Cliente/InformesOperativosView.java
@@ -0,0 +1,17 @@
+package Cliente;
+
+public class InformesOperativosView {
+
+	private String tipoInforme;
+	private InformeDTO informeData;
+
+	/**
+	 * 
+	 * @param tipo
+	 */
+	public void onGenerarInforme(int tipo) {
+		// TODO - implement InformesOperativosView.onGenerarInforme
+		throw new UnsupportedOperationException();
+	}
+
+}
