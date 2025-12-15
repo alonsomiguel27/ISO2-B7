2471277cb2eda9fb0cafaa3bb9a0eccfb9b0f267
arturodomenechh <arturo.domenech@alu.uclm.es>
2025-12-15 11:45:55 +0100

    Componente 25 - Cliente (CDU15) completado. Fixes #66

---
+++ Cliente/componente25_cdu15/src/main/java/es/UCLM/esi/ISO2/B7/GestionTutoriasView.java
@@ -0,0 +1,16 @@
+package es.UCLM.esi.ISO2.B7;
+
+public class GestionTutoriasView {
+
+    private int idTutoria;
+    private String fecha;
+    private String estado;
+
+    public void onSolicitarTutoria() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void onCancelarTutoria() {
+        throw new UnsupportedOperationException();
+    }
+}
