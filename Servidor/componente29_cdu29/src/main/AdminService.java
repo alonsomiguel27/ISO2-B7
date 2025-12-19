104ef1a2cc0dec919f3e3879ac5f57f8228db3d6
Maanu24sc <manuel.sanchez33@alu.uclm.es>
2025-12-19 15:19:43 +0100

    Implementación componente 29 Servidor #74.

---
+++ Servidor/componente29_cdu29/src/main/AdminService.java
@@ -0,0 +1,35 @@
+package Servidor;
+
+public class AdminService {
+
+	/**
+	 * 
+	 * @param clave
+	 * @param valor
+	 */
+	public void configurarParametro(int clave, int valor) {
+		// TODO - implement AdminService.configurarParametro
+		throw new UnsupportedOperationException();
+	}
+
+	public List<ParametroDTO> obtenerParametros() {
+		// TODO - implement AdminService.obtenerParametros
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param idRol
+	 * @param permisos
+	 */
+	public void gestionarRol(int idRol, int permisos) {
+		// TODO - implement AdminService.gestionarRol
+		throw new UnsupportedOperationException();
+	}
+
+	public List<LogDTO> verAuditoria() {
+		// TODO - implement AdminService.verAuditoria
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Servidor/componente29_cdu29/src/main/ParametroSistema.java
@@ -0,0 +1,18 @@
+package Servidor;
+
+public class ParámetroSistema {
+
+	private String clave;
+	private String valor;
+	private String descripcion;
+
+	/**
+	 * 
+	 * @param v
+	 */
+	public boolean validarValor(int v) {
+		// TODO - implement ParámetroSistema.validarValor
+		throw new UnsupportedOperationException();
+	}
+
+}
